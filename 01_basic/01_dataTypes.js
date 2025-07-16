1b2371c56e8d3269da9ed915ef62ea4d22a1db00
madhusmita23 <madhusmita.tech23@gmail.com>
2025-07-16 14:17:40 -0600

    basic

---
+++ 01_basic/01_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer version
+
+// alert("Hello"); //we are using nodeJs not browser
+let name ="Madhu" //string
+let age = 18 //Number, range is 2 to power 53
+let isLogedIn = true //boolean
+//bigint for bigger number
+let state = null //null ==> standalone value, representation with empty value
+let account; //undefined ==> not defined value in variable
+//symbol ==> unique
+
+//object
+console.log(typeof "Madhu") //string type
+console.log(typeof age) //number type
+console.log(typeof null) //object type 
+console.log(typeof undefined) //undefined type
